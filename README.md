390f3b95fad4d85e3d9905517174778996d2f43c
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2022-11-15 19:41:58 +0000

    change of most files each code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ICS2O-PWA-Template-HTML
+# ICS2O-Unit5-01
 
 [![Mr Coxall's Super Linter](https://github.com/sam-corbett/ICS2O-Unit5-01/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/sam-corbett/ICS2O-Unit5-01/actions)
 
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Sam Corbett All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Sam Corbett
+// Created on: Nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,14 +10,29 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-01/sw.js", {
+    scope: "/ICS2O-Unit5-01/",
   })
 }
 
+// This constant generates a random number.
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
 /**
- * This function displays an alert.
+ * This function updates the slider value.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+
+  // process of function
+  if (valueFromSlider == randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was: " + randomNumber + "! " + "Congratulations! You did it. " + "To Play Again, Please Restart The Page."
+  }
+  
+  // executed block of code, when the first condition is true 
+  if (valueFromSlider != randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was: " + randomNumber + "! " + "Sorry... " + "To Try Again, Please Restart The Page."
+  }
 }
